0dc8d2b2c0cf1893925131aa7f5de52525d7f028
Mikk0git <104311953+Mikk0git@users.noreply.github.com>
2023-12-13 12:48:24 +0100

    screen s made every 5 sec

--- dupa.py
+++ dupa.py
@@ -1,32 +1,7 @@
 import pyautogui
 import time
 import os 
-import smtplib
-from email.mime.text import MIMEText
-from email.mime.image import MIMEImage
-from email.mime.multipart import MIMEMultipart
 
-def SendMail(ImgFileName):
-   with open(ImgFileName, 'rb') as f:
-       img_data = f.read()
-
-   msg = MIMEMultipart()
-   msg['Subject'] = 'subject'
-   msg['From'] = 'dupagrygowskiego@fastmail.com'
-   msg['To'] = 'dupagrygowskiego@fastmail.com'
-
-   text = MIMEText("test")
-   msg.attach(text)
-   image = MIMEImage(img_data, name=os.path.basename(ImgFileName))
-   msg.attach(image)
-
-   s = smtplib.SMTP(Server, Port)
-   s.ehlo()
-   s.starttls()
-   s.ehlo()
-   s.login(UserName, UserPassword)
-   s.sendmail(From, To, msg.as_string())
-   s.quit()
 
 print("---")
 os.mkdir("tmp")
